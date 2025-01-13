19be5fd22aac23345a1f4f915afc16c396e7cf29
Nasir <nasiralishigri786@gmail.com>
2025-01-14 02:18:55 +0500

    changes to CRUD

--- anchor/programs/crud-app/src/lib.rs
+++ anchor/programs/crud-app/src/lib.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 declare_id!("5s3PtT8kLYCv1WEp6dSh3T7EuF35Z6jSu5Cvx4hWG79H");
 
 #[program]
-pub mod voting {
+pub mod crud {
     use super::*;
 
     pub fn create_journal_entry(ctx: Context<CreateEntry>, title: String, message:String) -> Result<()> {
